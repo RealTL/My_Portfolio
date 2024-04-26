18cfb77131e126d1e15903af2aa5b7fcb5514216
your name <you@examplendisorz3.com>
2024-04-26 14:14:54 -0500

    Week2.2 Smoke Test

---
+++ contracts/Token.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "My Token";
+    string symbol = "MTKN";
+    //uint8 decimals = 1000000 000000000000000000;
+
+}
+
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  },
 };
