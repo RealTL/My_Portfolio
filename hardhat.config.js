21fa1c957827d759ceed8ee5b44dce6f97f93c8f
your name <you@examplendisorz3.com>
2024-04-28 18:05:29 -0500

    DappU Week 2.8 - Token Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -15,8 +16,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    localhost: { url: "http://127.0.0.1:8545" },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(",")
     }
   },
+
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      avalanche: process.env.AVALANCHE_API_KEY,
+      avalancheFujiTestnet: process.env.AVALANCHE_API_KEY
+    }
+  }
+
 };
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -5,7 +5,7 @@ async function main() {
   const Token = await ethers.getContractFactory("Token");
 
   // Deploy contract
-  const token = await Token.deploy();
+  const token = await Token.deploy('DappU Test', 'DAPPT', '1000000');
   await token.deployed();
   console.log(`Token deployed to: ${token.address}`);
 
